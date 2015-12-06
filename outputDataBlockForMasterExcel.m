6840622bd2106e7ea70b2582e29de56d0a156f85
Brian <btran29@gmail.com>
2015-12-05 21:13:26 -0800

    added support for an arbitrary number of keywords, different file name delimiters, and cleaned up user input

--- outputDataBlockForMasterExcel.m
+++ outputDataBlockForMasterExcel.m
@@ -1,21 +1,20 @@
-% Ver 11-29-15 Brian
-%% Output transposed block of ramp data only
+% Ver 12-5-15 Brian
+%% Output transposed block of a column of data from studies w/ keywords
 % This script selects a column of data from all studies in a 
 % working directory, then outputs it all in a single copy-
-% paste-able block.
+% paste-able block in a workbook named after the first search term.
 
 % Usage:
-% Replace keyword with the study phase. If you want additional
-% granularity in the output, you can uncomment the variable
-% keyword2 and replace it with the phrase of your choice.
-% 	e.g. keyword2 = 'AmMa'
+% Set keyword to study phase: e.g. keyword = {'Baseline'}. For
+% multiple keywords, make it a list, e.g. keyword = {'Baseline',
+% 'Brain', 'AnSa'}.
 
 % To select final 2 minutes of baseline data for copying,
-% uncomment the code under: 
-% 	"% Select last 120 seconds for baseline data""
+% set last2min to 'true' (without apostrophes, case 
+% sensitive).
 
 
-%% Identify files of interest
+%% User input
 
 % Reset environment
 clear all
@@ -23,48 +22,55 @@ clear all
 % File extension
 fileExtension = '*.csv';
 
-% Keywords (e.g. study phase)
-keyword = 'Baseline';
-%keyword2 = 'AnSa';
+% Keywords (e.g. study phase, subject IDs)
+keyword = {'Baseline','Brain','AnSa'};
 
 % Column of data to select
 col = 3;
 
-% Collecting only last 120 seconds?
-baseln = true
+% Select last 12 samples
+last2min = true; % true or false, case sensitive
+
+% File name delimeter
+delimiter = ' ';
 
 % Max number of data points per study for variable of interest
 numData = 5000; % Some arbitrarily large value
 
+
+%% Identify files of interest
 % Generate file list
 fileType = dir(fileExtension);
 
 % Initialize index of files of interest
-idxAllFiles        = 1:size(fileType,1);
 idxFilesOfInterest = false(size(fileType,1),1);
 
 % Check if each file has keywords in its filename
 for iStudy = 1:length(fileType)
-    % If any filename includes the keyword
-    if any(strcmp(strsplit(fileType(iStudy,1).name,' '),keyword))
-        % Check for keyword 2, if it exists
-        if exist('keyword2','var') == 1
-			if any(strcmp(strsplit(fileType(iStudy,1).name,' '),keyword2)) 
-                % Mark studies that have the keyword
-                idxFilesOfInterest(iStudy) = true;
-                
-			end
-		else
-			% Mark studies that have the keyword
-            idxFilesOfInterest(iStudy) = true;
-            
-        end % end keyword2 conditional 
-    end % end keyword1 conditional
+
+% Initilize match list of keywords for each study
+matchList = false(length(keyword),1);
+
+    % Loop through all keywords, mark if study matches keyword
+    for iKeywordList = 1:length(matchList)
+
+        % Split filename into keywords by delimeter and compare
+        if any(strcmp(strsplit(fileType(iStudy,1).name,delimiter),keyword(iKeywordList)))
+            matchList(iKeywordList) = true;
+        end
+    end
+
+    % Mark studies that have the keyword
+    if all(matchList)
+        idxFilesOfInterest(iStudy) = true;
+    end
+ 
 end % end file loop
 
 % Warning if keyword not found
 if all(~idxFilesOfInterest)
     warning('No selected filetype with keyword(s) found')
+    return
 end
 
 %% Collect data from all files of interest 
@@ -74,21 +80,19 @@ datablock = cell(size(fileType,1),numData+1);
 
 % Loop over all files of interest (within filetype) in the directory
 for iFilesOfInterest = 1:length(fileType)
-    % Select variable column of interest
-    if idxFilesOfInterest(iFilesOfInterest) == true
+    % Select variable column of interest, transpose
+    if idxFilesOfInterest(iFilesOfInterest)
         importedFile = importdata(fileType(iFilesOfInterest).name);
         data = importedFile.data(:,col);
         data = num2cell(transpose(data)); 
-
-    % Select last 120 seconds for baseline data
-    if baseln == true
-      if size(data,2)>=12
-      	data = data((end-11):end); 
-    	else
-    	  warning('Study found with <12 data points')
-      end
-    end % end baseline conditional
-    
+	
+        % Select last 120 seconds for baseline data
+        if last2min
+            if size(data,2)>=12
+                data = data((end-11):end); 
+            end
+        end
+        
     % Enter study data into cell array shifted 1 col for study label
     datablock(iFilesOfInterest,1) = cellstr(...
         fileType(iFilesOfInterest).name);
@@ -101,4 +105,4 @@ end % end file loop
 datablock(all(cellfun(@isempty,datablock),2), : ) = [];
 
 %% Write cell array to an excel workbook file 
-xlswrite(strcat('output',keyword,'.xlsx'),datablock);
+xlswrite(strcat('output',keyword{1},'.xlsx'),datablock);
