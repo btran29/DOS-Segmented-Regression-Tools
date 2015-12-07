fed5c8769146c945733021c9f935f75f421bb70c
Brian <btran29@gmail.com>
2015-12-06 23:44:12 -0800

    Update segmented analysis main method.r

--- newer R-only workflow/analysis/segmented analysis main method.r
+++ newer R-only workflow/analysis/segmented analysis main method.r
@@ -10,14 +10,15 @@ source('dosSegmented.r') # Includes DOSI.segmented, linearize
 source('collectBPdata.r') # Includes collectBPdata, writeBPData, bpFigures 
 source('collectExeData.r') # Includes collectExeData, writeExeData
 
+
 # Organizing output into folders via labels (use for later output)
 breakpointDataFolder		<- "breakpoint data"
 percentExerciseDataFolder	<- "percent exercise data"
 segmentedFiguresFolder		<- "segmented figures"
-
 dir.create(breakpointDataFolder, showWarnings=FALSE)
 dir.create(percentExerciseDataFolder, showWarnings=FALSE)
 dir.create(segmentedFiguresFolder,showWarnings=FALSE)
+
 
 # List studies of interest
 csv			<- dir(pattern="*csv")
@@ -47,52 +48,52 @@ for(study in 1:length(dosStudies)){
 	exeData	<-	read.csv(csv[equivExeStudy])
 	
 	
-	# Assign data in files to data frames
-		# Assign DOS or EXE-specific time axes, normalizing DOS to begin at 0
-		Time     <- dosData$time
-		normTime <- Time-min(Time)
-		exeTime  <- exeData$time
+	# Assign data in files to data frames #
+	# Assign DOS or EXE-specific time axes, normalizing DOS to begin at 0
+	Time     <- dosData$time
+	normTime <- Time-min(Time)
+	exeTime  <- exeData$time
 
+	
+	# Make data frames from variables of interest
+	varDosData	<-	data.frame(dosData$HbO2,
+								dosData$HbR,
+								dosData$THb,
+								dosData$stO2)
+	varExeData <-	data.frame(exeData$VEO,
+								exeData$VEC,
+								exeData$PO,
+								exeData$PC,
+								exeData$VO2,
+								exeData$VCO2,
+								exeData$VE,
+								exeData$HR,
+								exeData$RR,
+								exeData$Work)
+	
+	
+	# Conver data to linear models (without any transformations), 
+	# using a dos or exe-specific time axis
+	lm.DOSdata	<- sapply(varDosData,linearize,xaxis=normTime,
+					simplify=FALSE,USE.NAMES = TRUE)
+	lm.EXEdata	<- sapply(varExeData,linearize,xaxis=exeTime,
+					simplify=FALSE,USE.NAMES = TRUE)
+	
+	
+	# Combine to run segmented
+	lin <- c(lm.DOSdata,lm.EXEdata)
 		
-		# Make data frames from variables of interest
-		varDosData	<-	data.frame(dosData$HbO2,
-									dosData$HbR,
-									dosData$THb,
-									dosData$stO2)
-		varExeData <-	data.frame(exeData$VEO,
-									exeData$VEC,
-									exeData$PO,
-									exeData$PC,
-									exeData$VO2,
-									exeData$VCO2,
-									exeData$VE,
-									exeData$HR,
-									exeData$RR,
-									exeData$Work)
 		
-		
-		# Conver data to linear models (without any transformations), 
-		# using a dos or exe-specific time axis
-		lm.DOSdata	<- sapply(varDosData,linearize,xaxis=normTime,
-						simplify=FALSE,USE.NAMES = TRUE)
-		lm.EXEdata	<- sapply(varExeData,linearize,xaxis=exeTime,
-						simplify=FALSE,USE.NAMES = TRUE)
-		
-		
-		# Combine to run segmented
-		lin <- c(lm.DOSdata,lm.EXEdata)
-		
-		
+	# Run segmented #
+	# Obtain breakpoints input for segmented function
+	segmentedMethod	<- table$SpecifSegmentedBPs[study]
+	segmentedBP1	<- table$FirstGuess[study]
+	segmentedBP2	<- table$SecondGuess[study]
+	
 	# Run segmented
-		# Obtain breakpoints input for segmented function
-		segmentedMethod	<- table$SpecifSegmentedBPs[study]
-		segmentedBP1	<- table$FirstGuess[study]
-		segmentedBP2	<- table$SecondGuess[study]
-		
-		# Run segmented
-		bpOutput	<- sapply(lin,DOSI.segmented,
-					segmentedMethod,segmentedBP1,segmentedBP2,
-					simplify=FALSE,USE.NAMES=TRUE)
+	bpOutput	<- sapply(lin,DOSI.segmented,
+				segmentedMethod,segmentedBP1,segmentedBP2,
+				simplify=FALSE,USE.NAMES=TRUE)
 		
 		
 	# Collect BP data
