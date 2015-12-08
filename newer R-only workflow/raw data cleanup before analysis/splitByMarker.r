32c9c0bf6a8d2cf7b7a7eb5e34e2c96fb07e793c
Brian <btran29@gmail.com>
2015-12-08 09:00:00 -0800

    fixed function; added edge case handling of duplicate markers

--- newer R-only workflow/raw data cleanup before analysis/splitByMarker.r
+++ newer R-only workflow/raw data cleanup before analysis/splitByMarker.r
@@ -11,7 +11,7 @@ splitByMarker <- function(keywords,batchFileDir,batchFile){
 
 	# Read table
   setwd(batchFileDir)
-	table <- read.table(batchFile, header=TRUE)
+	table <- read.csv(batchFile, header=TRUE)
   setwd("..")
 
 	# Locate all applicable files for threshold analysis
@@ -35,54 +35,61 @@ splitByMarker <- function(keywords,batchFileDir,batchFile){
 
 		# Load file, assumes CSV with a column called 'Marker'
 		data <- read.csv(csv[file])
-		indRampBeg	<-	which(data$Marker==rampBeg)
-		indRampEnd	<-	which(data$Marker==rampEnd)
-		indPedBeg	<-	which(data$Marker==pedBeg)
-		indRecovBeg	<-	which(data$Marker==recovBeg)
-		indPedEnd	<-	which(data$Marker==pedEnd)
+		indRampBeg	<-	which(data$Marker==rampBeg)[1] #First val only
+		indRampEnd	<-	which(data$Marker==rampEnd)[1] # if duplicate
+		indPedBeg	<-	which(data$Marker==pedBeg)[1]
+		indRecovBeg	<-	which(data$Marker==recovBeg)[1]
+		indPedEnd	<-	which(data$Marker==pedEnd)[1]
 
 
-		# Rearrange columns to match previous 15-column format,
+		# Rearrange columns to match previous 14-column format,
 		# assuming original TRS column-labels are kept
-		reorganized <-	data.frame(data$HbO2,data$Hb,data$tHb,data$SO2,
-						data$ElapsTime,data$Marker,data$SC1,data$SC2,
-						data$SC3,data$AC1,data$AC2,data$AC3,data$PL1,
-						data$PL2,data$PL3)
+		reorganized <-	data.frame(data$ElapsTime,data$HbO2,
+		                          data$Hb,data$tHb,data$SO2,
+						                  data$SC1,data$SC2,data$SC3,
+						                  data$AC1,data$AC2,data$AC3,
+						                  data$PL1,data$PL2,data$PL3)
 
 
 		# Define phases via marker data
 		allphases	<-	reorganized
-		baseline	<-	reorganized[1:(indRampBeg-1),]
-		ramp		<-	reorganized[indRampBeg:(indRampEnd-1),]
-		recovery	<-	reorganized[indRampEnd:(length(data[,1])),]
+		baseline	<-	reorganized[1:(indRampBeg[1]-1),]
+		ramp		<-	reorganized[indRampBeg[1]:(indRampEnd[1]-1),]
+		recovery	<-	reorganized[indRampEnd[1]:(length(data[,1])),]
 
 
 		# Bin Data
 		binSizes = 10/60 # 10 seconds over data in minutes
 
 		allphases.binned	<-	data.frame(sapply(allphases,uneqBinMeans,
-							binSize=binSizes,timeAxis=allphases$ElapsTime))
+							binSize=binSizes,timeAxis=allphases$data.ElapsTime))
 		baseline.binned		<-	data.frame(sapply(baseline,uneqBinMeans,
-							binSize=binSizes,timeAxis=baseline$ElapsTime))
+							binSize=binSizes,timeAxis=baseline$data.ElapsTime))
 		ramp.binned			<-	data.frame(sapply(ramp,uneqBinMeans,
-							binSize=binSizes,timeAxis=ramp$ElapsTime))
+							binSize=binSizes,timeAxis=ramp$data.ElapsTime))
 		recovery.binned		<-	data.frame(sapply(recovery,uneqBinMeans,
-							binSize=binSizes,timeAxis=recovery$ElapsTime))
+							binSize=binSizes,timeAxis=recovery$data.ElapsTime))
 
 
 		# Generate meaningful time axis for binned data
-		allphases.binned$ElapsTime <- seq(from=allphases.binned$ElapsTime[1],
-										  to=allphases.binned$ElapsTime[length(allphases.binned$ElapsTime)],
-										  by=10)
-		baseline.binned$ElapsTime <- seq(from=baseline.binned$ElapsTime[1],
-										  to=baseline.binned$ElapsTime[length(baseline.binned$ElapsTime)],
-										  by=10)
-		ramp.binned$ElapsTime <- seq(from=ramp.binned$ElapsTime[1],
-										  to=ramp.binned$ElapsTime[length(ramp.binned$ElapsTime)],
-										  by=10)
-		recovery.binned$ElapsTime <- seq(from=recovery.binned$ElapsTime[1],
-										  to=recovery.binned$ElapsTime[length(recovery.binned$ElapsTime)],
-										  by=10)
+
+		# New vars for code clarity
+		al <- allphases.binned$data.ElapsTime
+		ba <- baseline.binned$data.ElapsTime
+		ra <- ramp.binned$data.ElapsTime
+		re <- recovery.binned$data.ElapsTime
+
+		allphases.binned$data.ElapsTime <- seq(from=0,to=(length(al)*10)+10,
+		                                       by=10)[1:length(al)]
+
+		baseline.binned$data.ElapsTime <- seq(from=0,to=(length(ba)*10)+10,
+		                                      by=10)[1:length(ba)]
+
+		ramp.binned$data.ElapsTime <- seq(from=0,to=(length(ra)*10)+10,
+		                                  by=10)[1:length(ra)]
+
+		recovery.binned$data.ElapsTime <- seq(from=0,to=(length(re)*10)+10,
+		                                      by=10)[1:length(re)]
 
 
 		# Get current file for output file name
@@ -103,7 +110,7 @@ splitByMarker <- function(keywords,batchFileDir,batchFile){
 
 
 		# Output binned data
-		setwd("Data split by study phase")
+		setwd("Data split by study phase - binned")
 		write.table(allphases.binned,paste(outputFileName," All Phases - Binned",
 		".csv",sep=""),append=FALSE,row.names=FALSE,sep=",")
 		write.table(baseline.binned,paste(outputFileName," Baseline - Binned",
@@ -123,5 +130,5 @@ test <- FALSE
 if(test){
 # Should ouput 8 csv files with listed names, include studies with stated
 # keywords, and have fewer rows in the binned versions (check bins)
-splitByMarker("PFC|VL|Muscle|Brain",'study phase markers batch file.csv')
+splitByMarker("PFC|VL|Muscle|Brain",'batch files','study phase markers batch file.csv')
 }
