7a6f50a720d83f88819dfdc0ad9245517d4448b4
Brian <btran29@gmail.com>
2015-12-13 11:21:40 -0800

    fixed to run with 6310 renamed
    added 'hasExeData' conditional, removed 'linearized' sapply method in favor of individual labeling method method

--- newer R to segmented and SLM workflow/r segmented analysis/segmented analysis main method.r
+++ newer R to segmented and SLM workflow/r segmented analysis/segmented analysis main method.r
@@ -1,11 +1,19 @@
 # Ver 12-6-15 Brian
 # Main Method for data analysis
 
+
+# Input
+hasExeData = FALSE
+keyWords = "Brain"  # e.g. "PFC|VL|Muscle|Brain"
+seg.it = 10
+
+
 # Load required packages
 library(segmented)
 
 
 # Source functions for piecewise-linear regression analysis of DOS data
+# Assumes scripts are in the working directory. Manually source if need be
 source('exeEquivCSV.r') # Includes exeEquivCSV
 source('dosSegmented.r') # Includes DOSI.segmented, linearize
 source('collectBPdata.r') # Includes collectBPdata, writeBPData, bpFigures 
@@ -23,12 +31,13 @@ dir.create(segmentedFiguresFolder,showWarnings=FALSE)
 
 # List studies of interest
 csv			<- dir(pattern="*csv")
-dosStudies	<- csv[grep("PFC|VL|Muscle|Brain",csv)]
+csv			<- csv[grepl("Ramp",csv)]
+dosStudies	<- csv[grep(keyWords,csv)]
 
 
 # Obtain study-specific breakpoint guess inputs for segmented function
 setwd("batch files")
-	table <- read.csv("study phase markers batch file.csv")
+	table <- read.csv("segmentedBatchFile.csv")
 setwd("..")
 
 
@@ -36,18 +45,20 @@ setwd("..")
 for(study in 1:length(dosStudies)){
 
 	# Filename for output
-	outputFileName	<- dosStudies[study]
+	outputFileName	<- gsub(".csv", "", dosStudies[study])
 	
-	
+	if(hasExeData){
 	# Locate equivalent exercise study
 	equivExeStudy	<- exeEquivCSV(dosStudies[study],csv,
 					locEXO=1,locVisit=3,locDate=4)	
-	
+	}
 
 	# Read csv files into data tables
 	dosData	<-	read.csv(dosStudies[study])
-	exeData	<-	read.csv(csv[equivExeStudy])
 	
+	if(hasExeData){
+	exeData	<-	read.csv(csv[equivExeStudy])
+	}
 	
 	# Assign data in files to data frames #
 	
@@ -55,37 +66,61 @@ for(study in 1:length(dosStudies)){
 	# Assign DOS or EXE-specific time axes, normalizing DOS to begin at 0
 	Time     <- dosData$time
 	normTime <- Time-min(Time)
+	
+	if(hasExeData){
 	exeTime  <- exeData$time
-
+	}
 	
 	# Make data frames from variables of interest
-	varDosData	<-	data.frame(dosData$HbO2,
-								dosData$HbR,
-								dosData$THb,
-								dosData$stO2)
-	varExeData <-	data.frame(exeData$VEO,
-								exeData$VEC,
-								exeData$PO,
-								exeData$PC,
-								exeData$VO2,
-								exeData$VCO2,
-								exeData$VE,
-								exeData$HR,
-								exeData$RR,
-								exeData$Work)
-	
+		HbO2  <- data.frame(x=normTime, y=dosData$HbO2)
+		HbR   <- data.frame(x=normTime, y=dosData$HbR)
+		THb   <- data.frame(x=normTime, y=dosData$THb)
+		stO2  <- data.frame(x=normTime, y=dosData$stO2)
+		
+		if(hasExeData){
+		  VEO <- data.frame(x=exeTime, y=exeData$VEO)
+		  VEC <- data.frame(x=exeTime, y=exeData$VEC)
+		  PO  <- data.frame(x=exeTime, y=exeData$PO)
+		  PC  <- data.frame(x=exeTime, y=exeData$PC)
+		  VO  <- data.frame(x=exeTime, y=exeData$VO2)
+		  VOK <- data.frame(x=exeTime, y=exeData$VOK)
+		  VC  <- data.frame(x=exeTime, y=exeData$VCO2)
+		  VE  <- data.frame(x=exeTime, y=exeData$VE)
+		  HR  <- data.frame(x=exeTime, y=exeData$HR)
+		  RR  <- data.frame(x=exeTime, y=exeData$RR)
+		  RPM <- data.frame(x=exeTime, y=exeData$RPM)
+		  W   <- data.frame(x=exeTime, y=exeData$Work)
+		}
+
 	
 	# Conver data to linear models (without any transformations), 
 	# using a dos or exe-specific time axis
-	lm.DOSdata	<- sapply(varDosData,linearize,xaxis=normTime,
-					simplify=FALSE,USE.NAMES = TRUE)
-	lm.EXEdata	<- sapply(varExeData,linearize,xaxis=exeTime,
-					simplify=FALSE,USE.NAMES = TRUE)
-	
+	lin	<- list(
+	  "HbO2"  = lm.BO  <- lm(y~x,data=HbO2),
+	  "HbR"  = lm.BR  <- lm(y~x,data=HbR),
+	  "THb"  = lm.BT  <- lm(y~x,data=THb),
+	  "stO2"  = lm.BS  <- lm(y~x,data=stO2)
+	)
+
+	if(hasExeData){
+	  lm.EXEdata <- list(
+	    "VEO" = lm.VEO <- lm(y~x,data=VEO),
+	    "VEC" = lm.VC  <- lm(y~x,data=VEC),
+	    "PO"  = lm.PO  <- lm(y~x,data=PO),
+	    "PC"  = lm.PC  <- lm(y~x,data=PC),
+	    "VO"  = lm.VO  <- lm(y~x,data=VO),
+	    "VC"  = lm.VC  <- lm(y~x,data=VC),
+	    "VE"  = lm.VE  <- lm(y~x,data=VE),
+	    "HR"  = lm.HR  <- lm(y~x,data=HR),
+	    "RR"  = lm.RR  <- lm(y~x,data=RR),
+	    "RPM" = lm.RPM <- lm(y~x,data=RPM)
+	  )
+	}
 	
 	# Combine to run segmented
-	lin <- c(lm.DOSdata,lm.EXEdata)
-		
+	if(hasExeData){
+		lin <- c(lin,lm.EXEdata)
+	}	
 		
 	# Run segmented #
 	
@@ -101,8 +136,8 @@ for(study in 1:length(dosStudies)){
 				simplify=FALSE,USE.NAMES=TRUE)
 		
 		
-	# Collect and output BP data
-	
+	# Compare BP data with exe data
+	if(hasExeData){
 	
 	# Set span over which to mean exercise data for each breakpoint
 	span = (5/60) # Mean +/- 5 seconds, assuming data in minutes
@@ -110,12 +145,6 @@ for(study in 1:length(dosStudies)){
 	
 	# Output BP data using span and collectBPdata function
 	bpOutput2<-sapply(bpOutput,collectBPdata,span,simplify=FALSE,USE.NAMES=TRUE)
-	
-	
-	# Write segmented data into a csv file
-	setwd(breakpointDataFolder)
-		writeBPdata(bpOutput,bpOutput2,outputFileName)
-	setwd("..")
 	
 	
 	# Collect and output exercise data
@@ -127,20 +156,53 @@ for(study in 1:length(dosStudies)){
 		writeExeData(collectExeData(0.8,span=0),"E80",outputFileName)
 		writeExeData(collectExeData(1,span=0),"MaxWR",outputFileName)
 	setwd("..")
-		
-		
+	} else {
+		  bpOutput2 <-sapply(bpOutput,collectBPdata,span,hasExeData=FALSE,simplify=FALSE,USE.NAMES=TRUE)
+		}	
+	
+	
+	# Write segmented data into a csv file
+	setwd(breakpointDataFolder)
+		writeBPdata(bpOutput,bpOutput2,outputFileName)
+	setwd("..")
+	
+	
 	# Figures #
 
 	# Plot figures - Used try statements as some data is known to be too 
 	# noisy to all have breakpoints or successful
 	setwd(segmentedFiguresFolder)
-		pdf(paste(outputFileName," Figures.pdf",sep=""))
-		try({bpFigures(bpOutput$HbR,"Time (min)","[HbR] (uM)","PFC HbR")})
-		try({bpFigures(bpOutput$stO2,"Time (min)","stO2 (uM)","PFC stO2")})
-		try({bpFigures(bpOutput$HbO2,"Time (min)","HbO2 (uM)","PFC HbO2")})
-		try({bpFigures(bpOutput$THb,"Time (min)","THb (uM)","PFC THb")})
-		try({bpFigures(bpOutput$VE,"Time (min)","VE (L/min)","VE")})
-		try({bpFigures(bpOutput$PC,"Time (min)","PETCO2 mmHg","PETCO2")})
-		dev.off()
+	
+		tiffoutput <- function(VarName){
+  		  tiff(paste(VarName,outputFileName,"Figure.tiff",sep=" "), units = "px", width = 600, height = 600, res = NA, compression = "lzw")
+  		}
+
+  		tiffoutput("HbR")
+  		try({bpFigures(bpOutput$HbR,"Time (min)","[HbR] (uM)","PFC HbR")})
+  		dev.off()
+
+  		tiffoutput("stO2")
+  		try({bpFigures(bpOutput$stO2,"Time (min)","stO2 (uM)","PFC stO2")})
+  		dev.off()
+
+  		tiffoutput("HbO2")
+  		try({bpFigures(bpOutput$HbO2,"Time (min)","HbO2 (uM)","PFC HbO2")})
+  		dev.off()
+
+  		tiffoutput("THb")
+  		try({bpFigures(bpOutput$THb,"Time (min)","THb (uM)","PFC THb")})
+  		dev.off()
+
+  		if(hasExeData){
+  			try({bpFigures(bpOutput$VE,"Time (min)","VE (L/min)","VE")})
+  			try({bpFigures(bpOutput$PC,"Time (min)","PETCO2 mmHg","PETCO2")})
+  			}
+  			
 	setwd("..")
+	
+	
+	# Remove data for each study after output to tables and figures to prevent writing
+	# previous study's data to the next study
+	remove(bpOutput)
+	remove(bpOutput2)
 }
