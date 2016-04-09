5b1741adb408d7ce7dbd7ada28207e08a27600c5
Brian <btran29@gmail.com>
2016-04-09 11:37:15 -0700

    added simplified segmented workflow
    functional

---
+++ Matlab ver for pocketNIRS/simplified segmented for pocketNIRS/collectBPdata.r
@@ -0,0 +1,168 @@
+# Function to collect individual breakpoint data
+# Given a 'segmented' output for a particular variable:
+#	for every breakpoint:
+#		collect its X/Y-axis info, confidence intervals
+#		collect exercise data at equivlanet timepoint averaged
+#			+/- span (e.g. +/- 5 seconds)
+#		compile this data into a data frame
+# Output: data frame
+collectBPdata <- function(argv,span,hasExeData){
+  # Pre-allocate temporary vectors
+  # Segmented variables of interest
+  bpEstX      <- vector()
+  bpEstY      <- vector()
+  bpWork      <- vector()
+  bpRelTime   <- vector()
+  lConf       <- vector()
+  uConf       <- vector()
+  confDiff    <- vector()
+
+  # Exercise variables of interest
+  if(hasExeData){
+  bpAvgW        <- vector()
+  bpAvgWstDev   <- vector()
+  bpAvgVOK      <- vector()
+  bpAvgVOKstDev <- vector()
+  bpAvgHR       <- vector()
+  bpAvgHRstDev  <- vector()
+  bpAvgVE       <- vector()
+  bpAvgVEstDev  <- vector()
+
+
+  # Try statement ensures that there is an array for sapply, even if
+  # there is no applicable data
+  try({
+    if(length(argv$psi[,2])>=1){
+      for (iConfint in 1:length(argv$psi[,2])){
+        # Collect confint data
+        bpEstX[iConfint]   <- confint(argv)[[1]][[iConfint]]
+        bpEstY[iConfint]   <- argv$fitted.values[which(abs(argv$psi[iConfint,2]-argv$model$x)==
+								min(abs(argv$psi[iConfint,2]-argv$model$x)))]
+        lConf[iConfint]    <- confint(argv)[[1]][length(argv$psi[,2])*1+iConfint]
+        uConf[iConfint]    <- confint(argv)[[1]][length(argv$psi[,2])*2+iConfint]
+        confDiff[iConfint] <- abs(lConf[iConfint]-bpEstX[iConfint])
+
+        # Find equivalent exercise data by averaging exe data +/- 5 seconds noted by breakpoint
+        # Get equivalent time indicies +/- 5 seconds noted by breakpoint
+        equivExeTime       <- exeTime[which(abs(bpEstX[iConfint]-exeTime)==min(abs(bpEstX[iConfint]-exeTime)))]
+        indLequivExeTime   <- which(abs((equivExeTime-span)-exeTime)==min(abs((equivExeTime-span)-exeTime)))
+        indUequivExeTime   <- which(abs((equivExeTime+span)-exeTime)==min(abs((equivExeTime+span)-exeTime)))
+
+        # Average exe data over +/- 5 seconds from breakpoint
+        bpAvgW[iConfint]      <- mean(W$y[indLequivExeTime[1]:indUequivExeTime[1]])
+        bpAvgWstDev[iConfint] <- sd(W$y[indLequivExeTime[1]:indUequivExeTime[1]])
+
+        bpAvgVOK[iConfint]      <- mean(VOK$y[indLequivExeTime[1]:indUequivExeTime[1]])
+        bpAvgVOKstDev[iConfint] <- sd(VOK$y[indLequivExeTime[1]:indUequivExeTime[1]])
+
+        bpAvgHR[iConfint]      <- mean(HR$y[indLequivExeTime[1]:indUequivExeTime[1]])
+        bpAvgHRstDev[iConfint] <- sd(HR$y[indLequivExeTime[1]:indUequivExeTime[1]])
+
+        bpAvgVE[iConfint]      <- mean(VE$y[indLequivExeTime[1]:indUequivExeTime[1]])
+        bpAvgVEstDev[iConfint] <- sd(VE$y[indLequivExeTime[1]:indUequivExeTime[1]])
+
+        # Find breakpoint time/total ramp time
+        bpRelTime[iConfint] <- bpEstX[iConfint]/max(normTime)
+      }
+    }
+  })
+
+  # Collect information into a data frame
+  confintData <- data.frame(# Segmented variables of interest
+    bpEstX,bpEstY,lConf,uConf,confDiff,
+    # Exercises Variables of interest
+    bpAvgW,bpAvgWstDev,
+    bpAvgVOK,bpAvgVOKstDev,
+    bpAvgHR,bpAvgHRstDev,
+    bpAvgVE,bpAvgVEstDev,
+    bpRelTime)
+  return(confintData)
+  } else{
+   try({
+    # Try statement ensures that there is an array for sapply, even if
+    # there is no applicable data
+      if(!is.null(attributes(argv$psi)$dimnames[[2]][2])){
+        # Check for estimate "Est." attribute provided by 'segmented' with valid breakpoints
+
+        for (iConfint in 1:length(argv$psi[,2])){
+          # Collect confint data
+          bpEstX[iConfint]   <- confint(argv)[[1]][[iConfint]]
+          bpEstY[iConfint]   <- argv$fitted.values[which(abs(argv$psi[iConfint,2]-argv$model$x)==
+                                                           min(abs(argv$psi[iConfint,2]-argv$model$x)))]
+          lConf[iConfint]    <- confint(argv)[[1]][length(argv$psi[,2])*1+iConfint]
+          uConf[iConfint]    <- confint(argv)[[1]][length(argv$psi[,2])*2+iConfint]
+          confDiff[iConfint] <- abs(lConf[iConfint]-bpEstX[iConfint])
+
+          # Find breakpoint time/total ramp time
+          bpRelTime[iConfint] <- bpEstX[iConfint]/max(normTime)
+        }
+      }
+
+      confintData <- data.frame(
+        # Segmented variables of interest
+        bpEstX,
+        bpEstY,
+        lConf,
+        uConf,
+        confDiff,
+        bpRelTime)
+      return(confintData)
+    }) # end try statement
+  } # end hasExeData conditional
+}
+
+# Function to plot all breakpoints and stylize figures
+bpFigures <- function(variable,xAxisLabel,yAxisLabel,title){
+	# Variable data over a 12 minute time axis
+	plot(variable$model$x,variable$model$y, xlim=c(min(normTime),
+		(max(normTime))), ann=FALSE)
+	# Labels after base data points
+	mtext(side = 1, text = xAxisLabel, line = 2)
+	mtext(side = 2, text = yAxisLabel, line = 2)
+	mtext(side = 3, text = title, line = 0, font = 2)
+	# Segmented data
+	lines(variable$model$x,variable$fitted.values)
+	lines(variable,lwd = 2, col= 553, lty =1,shift = TRUE, pch = 0,cex=10, k =1.8)
+	# Vertical Lines
+	for(iLine in 1:(length(variable$psi)/3)){
+	  abline(v =variable$psi[iLine,2] , untf = FALSE, lty=2)
+	}
+}
+
+# Test code #
+test <- FALSE
+if(test){
+	# REQUIRES 'segmented' to be loaded in order to interpret loaded sample
+	# breakpoint data
+	library(segmented)
+	# Create pre-requisite sample data
+	csv <- dir(pattern="*csv")
+
+	# Assign Data
+	csvExeData		<- read.csv(csv[1],header = TRUE)
+	normTime	<- csvExeData[[1]]-min(csvExeData[[1]])
+	exeTime	<- csvExeData[[1]]
+	VOK		<- data.frame(x=exeTime, y=csvExeData[[7]])
+	VE		<- data.frame(x=exeTime, y=csvExeData[[9]])
+	HR		<- data.frame(x=exeTime, y=csvExeData[[10]])
+	W		<- data.frame(x=exeTime, y=csvExeData[[13]])
+
+	outputFileName <- "EXO-1 AaAa V3 11-11-11 "
+
+
+	load("bpOutput")
+
+	# Call function over all variables
+	bpOutput2<-sapply(bpOutput,collectBPdata,simplify=FALSE,USE.NAMES=TRUE)
+
+
+	# Write segmented data into a text file
+	for(ibpOutput in 1:length(bpOutput2)){
+	  write.table(paste(names(bpOutput)[ibpOutput]),
+				  paste(outputFileName,"Data.txt",sep=""), sep="\t", append=TRUE,
+				  row.names=FALSE)
+	  write.table(bpOutput2[ibpOutput],
+				  paste(outputFileName,"Data.txt",sep=""), sep="\t", append=TRUE,
+				  row.names=FALSE)
+	}
+}
---
+++ Matlab ver for pocketNIRS/simplified segmented for pocketNIRS/pocketNIRSseg.R
@@ -0,0 +1,103 @@
+## Set up initial files ##
+# List studies of interest
+directory	<- dir(pattern="*.csv")
+
+# Set data file of interest
+dosData.fid <- directory[2]
+
+# Grab data from pre-formatted CSV file
+# Each testing session is one column, time is on axis
+# Time axis is normalized to start of testing session and shared across sessions
+dosdata <- read.csv(dosData.fid)
+dosdata.numberofstudies <-length(colnames(dosdata)[-1])
+
+# Segmented input table for each session
+# Four column long-style table
+default <- vector(mode = "numeric",length = length(dosdata.numberofstudies))
+seg.input.fid <- paste("segmentedInput",dosData.fid,sep="_")
+table   <- data.frame(file=colnames(dosdata)[-1],
+                      specifiedBPs=default,
+                      segBP1=default)
+write.table(table,seg.input.fid,append=FALSE,row.names=FALSE,sep=",")
+
+# Set segmented input file of interest
+directory	<- dir(pattern="*.csv")
+seg.input.fid <- directory[4]
+
+
+
+## Analysis ##
+# Read input
+seg.it <- 10
+seg.input <- read.csv(seg.input.fid)
+
+# Initialize breakpoint collection variable
+bp.output <- replicate(dosdata.numberofstudies, list()) 
+#bp.output <- vector(mode = "list", length = dosdata.numberofstudies)
+
+# Collect intial guessing data for segmented
+# Workflow note: play with values until segmented works for a session
+# If no values work/data is undecipherable, leave at 0 specified BPs
+# Expand loop to eventually cover entire number of available studies
+for (session in 1:dosdata.numberofstudies){
+
+  var.column <- session+1
+  var <- data.frame(x=dosdata$Time, y=dosdata[,var.column])
+  var.lm  <- lm(y~x,data=var)
+
+  # Run segmented into collection variable
+  try({
+    if (seg.input$specifiedBPs[session] == 0){
+      seg.out <- segmented(var.lm,seg.Z=~x,psi=list(x=NA),
+                          control=seg.control(stop.if.error=FALSE,n.boot=0,
+                                              it.max=seg.it))
+    } else if (seg.input$specifiedBPs[session] == 1){
+      seg.out <- segmented(var.lm,seg.Z=~x,psi=list(x=c(seg.input$segBP1[session])),
+                           control=seg.control(display=FALSE,n.boot=50,
+                                               it.max=seg.it))
+    } # end specified bp conditionals
+  }) # end try statement
+  
+  bp.output[[session]] <- seg.out #Save seg.out (list datatype) into bp.output list
+  rm(seg.out,var.lm)
+}
+
+# Collect BP Data, using collectBPdata function
+normTime <- dosdata$Time # required by collectBPdata
+bp.outputlist <-sapply(bp.output,collectBPdata,span,hasExeData=FALSE,simplify=FALSE,USE.NAMES=TRUE)
+
+# Remove data for sessions with >1 bp
+for (session in 1:dosdata.numberofstudies){
+  if (length(bp.outputlist[[session]]$bpEstX)>1){
+    bp.outputlist[[session]] <- data.frame(bpEstX=double(),
+                                           bpEstY=double(),
+                                           lConf=double(),
+                                           uConf=double(),
+                                           bpRelTime=double())
+  }
+}
+
+# Make list of file names
+session.fid <- vector(mode="character", length = dosdata.numberofstudies) #Initialize empty vector
+for (session in 1:dosdata.numberofstudies){
+  seg.out.fid <-  paste("segmentedOutput",dosData.fid,sep="_")
+  session.fid[[session]] <- colnames(dosdata)[session+1] # Skip time in column names
+}
+
+# Concatenate file names 
+for (session in 1:dosdata.numberofstudies){
+  if (length(bp.outputlist[[session]]$bpEstX)==1){
+  bp.outputlist[[session]]$file <- session.fid[[session]]
+  bp.outputlist[[session]] <-  bp.outputlist[[session]][c(7,1,2,3,4,5,6)] # file names to 1st column
+  }
+}
+
+# Write to table
+for (session in 1:dosdata.numberofstudies){
+  seg.out.fid <-  paste("segmentedOutput",dosData.fid,sep="_")
+  if(session==1){ # Create column names with first set of data
+    write.table(bp.outputlist[[session]],file=seg.out.fid,append=T, sep=",",row.names = F)  
+  } else if(session>1){ # Just write row w/out column names
+    write.table(bp.outputlist[[session]],file=seg.out.fid,append=T, sep=",",row.names = F,col.names=F)
+  }
+}
