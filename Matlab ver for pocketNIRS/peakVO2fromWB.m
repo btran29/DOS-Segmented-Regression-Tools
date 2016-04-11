361a19dbd9315e32e42c3306faa09c77e5f0806f
Brian <btran29@gmail.com>
2016-04-11 13:50:57 -0700

    added additional outputs for met cart processing
    functional

--- Matlab ver for pocketNIRS/peakVO2fromWB.m
+++ Matlab ver for pocketNIRS/peakVO2fromWB.m
@@ -55,7 +55,14 @@ endRows = [529
 peakVO2 = zeros(25,1);
 peakRQ  = zeros(25,1);
 peakHR  = zeros(25,1);
-peakHalfVO2 = zeros(25,1);
+time_halfpeakVO2 = zeros(25,1);
+
+halfpeak_HR = zeros(25,1);
+halfpeak_Work = zeros(25,1);
+halfpeak_VO2 = zeros(25,1);
+halfpeak_VO2kg = zeros(25,1);
+halfpeak_VCO2 = zeros(25,1);
+halfpeak_RQ = zeros(25,1);
 
 coef_HR = zeros(25,2);
 coef_Work = zeros(25,2);
@@ -145,7 +152,7 @@ for iFile = 1:25
     % Determine time at 50% VO2 peak, called 'TimeSec_exe_halfpeak'
     [~,idx_peakVO2] = max(windowmean);
     idx_halfpeakVO2 = ceil(idx_peakVO2*0.5);
-    peakHalfVO2(iFile) = TimeSec_exe(idx_halfpeakVO2);
+    time_halfpeakVO2(iFile) = TimeSec_exe(idx_halfpeakVO2);
     
     
     % Get slope of data from 0 to 50% VO2 peak
@@ -162,7 +169,15 @@ for iFile = 1:25
     coef_RQ(iFile,1:2) = polyfit(TimeSec_exe(1:idx_halfpeakVO2),...
                     exe_RQ(1:idx_halfpeakVO2),1);
                  
-                
+    
+    % Get value at 50% peak VO2 for data of interest
+    halfpeak_HR(iFile) = exe_HR(idx_halfpeakVO2);
+    halfpeak_Work(iFile) = exe_Work(idx_halfpeakVO2);
+    halfpeak_VO2(iFile) = exe_VO2(idx_halfpeakVO2);
+    halfpeak_VO2kg(iFile) = exe_VO2kg(idx_halfpeakVO2);
+    halfpeak_VCO2(iFile) = exe_VCO2(idx_halfpeakVO2);
+    halfpeak_RQ(iFile) = exe_RQ(idx_halfpeakVO2);
+    
     % Get delta value for data of interest from other variables
     delta_HR(iFile) = exe_HR(idx_halfpeakVO2)-exe_HR(1);
     delta_Work(iFile) = exe_Work(idx_halfpeakVO2)-exe_Work(1);
@@ -176,12 +191,14 @@ end % end file loop
 
 %% Combine
 T_Input = dataset(endRows,'ObsNames',sheetNames);
-T1 = dataset(peakVO2,peakRQ,peakHR,peakHalfVO2,'ObsNames',sheetNames);
-T2 = dataset(coef_HR,coef_Work,coef_VO2,coef_VO2kg,coef_VCO2,coef_RQ,...
-    'ObsNames',sheetNames);
+T1 = dataset(peakVO2,peakRQ,peakHR,time_halfpeakVO2,'ObsNames',sheetNames);
+T2 = dataset(halfpeak_HR,halfpeak_Work,halfpeak_VO2,halfpeak_VO2kg,...
+    halfpeak_VCO2,halfpeak_RQ,'ObsNames',sheetNames);
 T3 = dataset(delta_HR,delta_Work,delta_VO2,delta_VO2kg,delta_VCO2,delta_RQ,...
     'ObsNames',sheetNames);
-T_output = horzcat(T_Input,T1,T2,T3);
+T4 = dataset(coef_HR,coef_Work,coef_VO2,coef_VO2kg,coef_VCO2,coef_RQ,...
+    'ObsNames',sheetNames);
+T_output = horzcat(T_Input,T1,T2,T3,T4);
 
 %% Output to excel
 output_fid = 'exOutput.csv';
