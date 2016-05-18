4eb27ca89a35c9d4795270f484e9c1c1562e34d8
Brian <btran29@gmail.com>
2016-05-18 00:15:54 -0700

    edited function comments for clarity

--- Matlab ver for pocketNIRS/getsubjectID.m
+++ Matlab ver for pocketNIRS/getsubjectID.m
@@ -2,11 +2,18 @@ function [ subjectIdentifier ] = getsubjectID( filename )
 %LDF Summary of this function goes here
 % Find subject IDs via regex and multiple expressions. Expressions are
 % tried sequentially.
-% Format for which the current expressions work for:
-%   subject IDs are 3 or 4 digit numbers
-%   pocket_nirs_log_20160101_13.23.19_999_AA_CH1_right_CH2_left_15WR.PNI
-%   pocket_nirs_log_20160101_13_03_39_Test_9999_AA_CH1_left_CH2_right_WR15.PNI
-%   9999_AA_CH1right_CH2left_15wts_pocket_nirs_log_20160101_13.23.19.PNI
+%
+% Input
+% 	1. filename, string
+%   Formats which the current expressions work with:
+%   	subject IDs are 3 or 4 digit numbers
+%  		pocket_nirs_log_20160101_13.23.19_999_AA_CH1_right_CH2_left_15WR.PNI
+%  		pocket_nirs_log_20160101_13_03_39_Test_9999_AA_CH1_left_CH2_right_WR15.PNI
+%  		9999_AA_CH1right_CH2left_15wts_pocket_nirs_log_20160101_13.23.19.PNI
+%
+% Output
+%	1. subjectIdentifier, string
+%
 
 % Rules for cleaning up file names
 expression = '\d\d\d\d_[A-Z][A-Z]_'; % Search for ID + 2 lett initials
--- Matlab ver for pocketNIRS/unequalsizedbins.m
+++ Matlab ver for pocketNIRS/unequalsizedbins.m
@@ -2,6 +2,19 @@ function [ binMeans ] = unequalsizedbins( interval,data,time )
 %UNEQUALSIZEDBINS Bins data over a given interval given time and data
 %   Data point count in each bin is determined by the given interval
 %   Interval is in the units of time as specified by the time axis
+%
+% Inputs
+%	1. interval, numeric
+%		interval over given time-axis to bin over
+%	2. data, numeric array
+%		y-axis data of which to bin
+%	3. time, numeric array
+%		x-axis data representing time-axis
+%
+% Output
+%	1. last10secBinMeans, numeric array
+%		bin every 10 seconds, using +/- interval to calculate each bin mean
+%
 
 % Define bin edges
 binEdges = 0:interval:(time(end)+interval); % add an extra step
